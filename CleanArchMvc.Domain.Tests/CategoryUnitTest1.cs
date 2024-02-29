ce6f30aa92960ed83139db2b082a49407cdb029b
erik <erik.fernandess@hotmail.com>
2024-02-29 08:17:44 -0300

    inicio

---
+++ CleanArchMvc.Domain.Tests/CategoryUnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using CleanArchMvc.Domain.Entities;
+using FluentAssertions;
+using Xunit;
+
+namespace CleanArchMvc.Domain.Tests
+{
+    public class CategoryUnitTest1
+    {
+        [Fact(DisplayName ="Create category with valid state")]
+        public void CreateCategory_WithValidParameters_ResultObjectValidState()
+        {
+            Action action= () => new Category(1, "Category Name");
+            action.Should()
+                .NotThrow<CleanArchMvc.Domain.Validation.DomainExceptionValidation>();
+        }
+
+
+        [Fact(DisplayName = "Create category with invalid state")]
+        public void CreateCategory_NegativeValue_DomainExceptionInvalid()
+        {
+            Action action = () => new Category(-1, "Category Name");
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid Id Value");
+        }
+    }
+}
---
+++ CleanArchMvc.Domain/Entities/Product.cs
@@ -0,0 +1,69 @@
+﻿using CleanArchMvc.Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public sealed class Product : Entity
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public int Stock { get; private set; }
+        public string Image { get; private set; }
+
+        public Product(string name, string description, decimal price, int stock, string image)
+        {
+            ValidateDomain(name, description, price, stock, image);
+        }
+
+        public Product(int id, string name, string description, decimal price, int stock, string image)
+        {
+            DomainExceptionValidation.When(id < 0,
+            "Invalid id value");
+            Id = id;
+
+            ValidateDomain(name, description, price, stock, image);
+        }
+
+        public void Update(string name, string description, decimal price, int stock, string image)
+        {
+            ValidateDomain(name, description, price, stock, image);
+            CategoryId = CategoryId;
+        }
+
+        private void ValidateDomain(string name, string description, decimal price, int stock, string image)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+            "Invalid Name is required");
+
+            DomainExceptionValidation.When(name.Length < 3,
+           "Invalid Name is required");
+
+            DomainExceptionValidation.When(string.IsNullOrEmpty(description),
+            "Invalid description is required");
+
+            DomainExceptionValidation.When(name.Length < 5,
+           "Invalid description is required");
+
+            DomainExceptionValidation.When(string.IsNullOrEmpty(image),
+           "Invalid Name is required");
+
+            DomainExceptionValidation.When(name.Length > 250,
+           "Invalid Name is required");
+
+            DomainExceptionValidation.When(price < 0,
+           "invalid price value");
+
+            DomainExceptionValidation.When(stock < 0,
+           "invalid stock value");
+        }
+
+        //chave estrangeira
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
